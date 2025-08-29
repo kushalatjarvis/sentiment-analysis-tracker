5f48c70ec10e25de96eb68e438f6cab708d09ad5
Kushal Nandwani <kushal@jarvisatwork.com>
2025-08-29 10:47:17 +0530

    fixed the styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="py-12">
       {/* Hero */}
       <section className="max-w-3xl mx-auto flex flex-col items-center justify-center gap-4 mt-16 text-center">
-        <h1 className="text-3xl sm:text-7xl font-black sm:mt-2 mt-12">
+        <h1 className="text-4xl sm:text-7xl font-black sm:mt-2 mt-12">
           Sentiment Analysis Tracker
         </h1>
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header className="w-full py-6 px-2 sm:px-8 fixed top-0 z-50 bg-black text-primary-foreground">
+    <header className="w-full py-6 fixed top-0 z-50 bg-black text-primary-foreground">
       <nav className="sm:flex items-center justify-between w-full my-container">
         <Link
           className="text-2xl font-bold flex items-center justify-center gap-1"
